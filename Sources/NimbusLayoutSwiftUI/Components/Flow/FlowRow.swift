fecaa540d3593110bb87e7a91e7ea66dffe8ef86
Arthur Bleil <arthur.bleil@zup.com.br>
2022-11-30 18:07:03 -0300

    feat: flow column and flow row
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>

---
+++ Sources/NimbusLayoutSwiftUI/Components/Flow/FlowRow.swift
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2022 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import SwiftUI
+import NimbusSwiftUI
+
+struct FlowRow<Content: View>: View, Decodable {
+  @Children var children: () -> Content
+  @Root var box: Box
+  
+  var body: some View {
+    if #available(iOS 14.0, *) {
+      FlowLayout(axis: .horizontal, alignment: Alignment(horizontal: .leading, vertical: .top), content: children)
+        .modifier(BoxModifier(box: box))
+    }
+  }
+}
