44b2564410f3278aaa93745143f1da7530baef43
Tiago Peres França <tiago.franca@zup.com.br>
2023-01-17 17:17:58 -0300

    releases 1.0.0-beta.1

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/ZupIT/nimbus-swiftui.git",
-      from: "1.0.0-alpha.2"
+      from: "1.0.0-beta.1"
     )
   ],
   targets: [
